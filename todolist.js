8d9db4230358c160daf84ef9cafce1fa01cd769b
CodeMeYulia <yupipper@gmail.com>
2023-11-29 05:46:04 +0300

    add check empty

--- todolist.js
+++ todolist.js
@@ -6,15 +6,19 @@ const todolist = document.getElementById('todolist');
 
 //функция добавления новой задачи
 function addNewTask() {
+if (newTask.value.trim().length){
    const taskPoint = document.createElement('li');
    taskPoint.innerText = newTask.value;
    todolist.append(taskPoint);
    newTask.value = "";
 
+   //зачеркиваем выполненное
    taskPoint.addEventListener('click', function(){
+    console.log(e.target.tagName);
     taskPoint.classList.toggle('close');
 });
-}
+}else{alert("добавьте задачу")};
+} ;
 
 //вешаем обработчик события на кнопку добавления задачи по клику
 addTask.addEventListener('click', addNewTask);
